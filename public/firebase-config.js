43db8192ca183d612efc3d43b7b6499a412dd1a4
gabriellsd <gabrielsilvadis@outlook.com>
2025-06-02 01:16:09 -0300

    feat: adiciona scripts Firebase aos arquivos HTML e configuração

---
+++ public/firebase-config.js
@@ -0,0 +1,16 @@
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBrYy4QDCNs8MJsUuScvNWnwt4uoAEsZjM",
+    authDomain: "gdfinances-16586.firebaseapp.com",
+    projectId: "gdfinances-16586",
+    storageBucket: "gdfinances-16586.firebasestorage.app",
+    messagingSenderId: "317414845406",
+    appId: "1:317414845406:web:e9f9579fe78a31e9cfcb4f"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Get Firebase services
+const auth = firebase.auth();
+const firestoreDB = firebase.firestore(); 
